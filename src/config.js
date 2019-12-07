ffb29fb88078c3a545d7ac669373a289d1ba336a
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2019-12-07 23:39:08 +0300

    * API

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8081";
+export const API_URL = "http://195.161.114.197:8081";
 export const MAP_URL =
-  "https://yandex.ru/maps/213/moscow/?ll=37.622504%2C55.753215&mode=routes&rtext=";
+    "https://yandex.ru/maps/213/moscow/?ll=37.622504%2C55.753215&mode=routes&rtext=";
