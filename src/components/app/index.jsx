f8682379b6c3dc626c87c26930f8b330ae65b6ab
Alexey.f <a.fokin@pflb.ru>
2019-12-07 23:46:35 +0300

    Fixed path case

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 
-import "./app.css";
+import "./App.css";
 
 import rootSaga from "../../workflows/sagas";
 import createReduxStore from "../../workflows/reducers/index";
@@ -10,7 +10,7 @@ import Router from "../Router";
 
 import theme from "../../theme";
 
-import {ThemeProvider} from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 const store = createReduxStore();
 store.runSaga(rootSaga);
